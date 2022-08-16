9fbd043c249d92da827a987112f5a9aa5ddc4d78
Tim Mecklem <timothy@mecklem.com>
2022-08-16 01:02:16 -0400

    One solution

--- lib/cincy_rbot/dice_roller.rb
+++ lib/cincy_rbot/dice_roller.rb
@@ -4,6 +4,12 @@ module CincyRbot
   class DiceRoller
     class << self
       def register_bot_events(bot)
+        bot.message(contains: /roll a|(the) dic?e/) { |event| roll_a_die(event) }
+        bot.mention(contains: /re-roll/) { |event| roll_a_die(event) }
+      end
+
+      def roll_a_die(event)
+        event.respond "You rolled #{rand(1...6)}"
       end
     end
   end
